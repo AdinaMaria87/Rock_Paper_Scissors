c2579cc955d6a383cf44d532f4163549ae7c211e
AdinaMaria87 <iacob_adina_maria87@yahoo.com>
2023-05-05 03:47:30 +0300

    Create game() function and modify previous function playRound

--- script.js
+++ script.js
@@ -17,29 +17,47 @@ function playRound(playerSelection, computerSelection) {
     } 
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         scorePlayer++;
-        console.log("You win! Rock beats scissors!" + " Your score is:" + scorePlayer + " and computer score is: " + scoreComputer);
+        console.log("You win! Rock beats scissors!" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
     }
     else if(playerSelection == "paper" && computerSelection == "rock") {
         scorePlayer++;
-        console.log("You win! Paper beats rock!" + " Your score is:" + scorePlayer + " and computer score is: " + scoreComputer);
+        console.log("You win! Paper beats rock!" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
        
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         scoreComputer++;
-        console.log("You lost! Scissors beat paper!" + " Your score is:" + scorePlayer + " and computer score is: " + scoreComputer);
+        console.log("You lost! Scissors beat paper!" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         scorePlayer = scorePlayer++;
-        console.log("You win! Scissors beat paper" + " Your score is:" + scorePlayer + " and computer score is: " + scoreComputer);
+        console.log("You win! Scissors beat paper" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
     }
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         scoreComputer++;
-        console.log("Yoy lost! Rock beats scissors!" + " Your score is:" + scorePlayer + " and computer score is: " + scoreComputer);
+        console.log("You lost! Rock beats scissors!" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
     }
     else {
-        console.log("It's equality" + "Your choice:" + playerSelection + "Computer choice:" + computerSelection);
+        console.log("It's equality" + " Your score is:" + " " + scorePlayer + " and computer score is: " + scoreComputer);
     }
-  
+
 }
 
 
+function game() {
+    
+    for(let i=1; i<=5; i++) {
+        let playerSelection = prompt("Make your choice: rock, paper or scissors?").toLocaleLowerCase();
+        let computerSelection = getComputerChoice().toLocaleLowerCase();
+        playRound(playerSelection, computerSelection);
+        }
+    if(scorePlayer < scoreComputer) {
+         console.log("You lost!")
+        }
+    else if(scorePlayer > scoreComputer) {
+        console.log("You win!");
+    }
+    else {
+        console.log("It's equality!");
+    }
+}
+game();
